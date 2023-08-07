1b314075c48ccdcf78d01fb3103d099f20069c0b
Odysseas.eth <odyslam@gmail.com>
2023-08-07 23:41:30 +0200

    chore: comments

--- src/Alert.sol
+++ src/Alert.sol
@@ -1,5 +1,11 @@
-import {Test} from "forge-std/Test.sol";
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.9.0;
+pragma experimental ABIEncoderV2;
 
-abstract contract Alert is Test {}
+import {Test} from "forge-std/Test.sol";
+import {Phylax} from "./Phylax.sol";
+
+// Base contract for all Phylax alert contracts.
+abstract contract Alert is Test {
+    Phylax internal constant phylax = Phylax(VM_ADDRESS);
+}
