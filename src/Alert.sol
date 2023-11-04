0e33c7d64cceb1b34c906c07b804ca3b7d04ac1a
Odysseas.eth <odyslam@gmail.com>
2023-11-04 12:52:23 +0200

    feat: modifiers to enable fork and log block number

--- src/Alert.sol
+++ src/Alert.sol
@@ -7,5 +7,20 @@ import {Phylax} from "./Phylax.sol";
 
 // Base contract for all Phylax alert contracts.
 abstract contract Alert is Test {
-    Phylax internal constant phylax = Phylax(VM_ADDRESS);
+    Phylax internal constant ph = Phylax(VM_ADDRESS);
+
+    uint256[] activeChains;
+
+    function enableChain(
+        string calldata aliasOrUrl
+    ) internal returns (uint256) {
+        activeChains.push(vm.createFork(aliasOrUrl));
+        return activeChains.length - 1;
+    }
+
+    modifier chain(uint256 index) {
+        ph.export("fork_activated", "");
+        vm.selectFork(activeChains[index]);
+        _;
+    }
 }
