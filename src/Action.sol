19357ab7ea8ac5938408728ab9df31cb9eb0ec48
Odysseas.eth <odyslam@gmail.com>
2023-11-26 21:24:36 +0200

    ref: move forking codes to commonbase
    Also, when enabling a chain, it will try to setup the phylax contract.
    
    The phylax contract is like the vm, but in case the cheatcode revets, it just logs it.
    
    new versions of foundry revert with unknown cheatcodes

--- src/Action.sol
+++ src/Action.sol
@@ -2,10 +2,11 @@
 pragma solidity >=0.6.2 <0.9.0;
 pragma experimental ABIEncoderV2;
 
-import {Script} from "forge-std/Script.sol";
-import {Phylax} from "./Phylax.sol";
+import { Script } from "forge-std/Script.sol";
+import { PhylaxBase } from "./PhylaxBase.sol";
 
-// Base contract for all Phylax alert contracts.
-abstract contract Action is Script {
-    Phylax internal constant ph = Phylax(VM_ADDRESS);
+/// @title Alert
+/// @dev Base contract for all Phylax alert contracts.
+abstract contract Action is PhylaxBase, Script {
+
 }
