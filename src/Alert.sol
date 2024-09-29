2c13f419681609ac8300884ea39c65d3777c0a6a
Qwuke <tristan.e.mortimer@gmail.com>
2024-09-29 16:11:59 -0400

    Do some slight cleanup

--- src/Alert.sol
+++ src/Alert.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.2 <0.9.0;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 import {Test} from "forge-std/Test.sol";
 import {PhylaxBase} from "./PhylaxBase.sol";
 import {PhylaxNotification} from "./PhylaxNotification.sol";
+import {PhylaxCharts} from "./PhylaxCharts.sol";
 
 /// @title Alert
 /// @dev Base contract for all Phylax alert contracts.
-abstract contract Alert is PhylaxBase, PhylaxNotification, Test {}
+abstract contract Alert is PhylaxBase, PhylaxCharts, PhylaxNotification, Test {}
